4e02a13b4651379d2c320bac00768e88f4a03f3b
leozhou <492753662@qq.com>
2025-02-15 19:21:55 +0800

    feaT: hide footer in docs

--- src/app/_meta.js
+++ src/app/_meta.js
@@ -6,11 +6,19 @@ const meta = {
   docs: {
     type: "page",
     title: "Documentation",
+    theme: {
+      footer: false,
+    },
   },
   showcase: {
     type: "page",
     title: "Showcase",
   },
+  // test: {
+  //   type: "page",
+  //   title: "Test",
+  //   href: "/docs/cases",
+  // },
 };
 
 export default meta;
