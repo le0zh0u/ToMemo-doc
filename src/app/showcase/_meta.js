fad27f6749b48a061a408b40fbb8b7ce81b596b5
leozhou <492753662@qq.com>
2025-02-15 19:36:15 +0800

    feat: update docs

---
+++ src/app/showcase/_meta.js
@@ -0,0 +1,14 @@
+const meta = {
+  index: {
+    theme: {
+      footer: false,
+      sidebar: false,
+      toc: false,
+      timestamp: false,
+      breadcrumb: false,
+      collapsed: true,
+    },
+  },
+};
+
+export default meta;
